7f28198df0b43f1bef9978bbb374a53521728fbc
Giuseppe D'Ambrosio <giuseppe.dambrosio14@gmail.com>
2021-02-21 15:39:44 +0100

    fix for ubuntu16.04

--- ComputeCPPInstall.sh
+++ ComputeCPPInstall.sh
@@ -6,11 +6,13 @@
 sudo apt update -y
 sudo apt upgrade -y
 sudo apt install -y build-essential autoconf cmake unzip
-sudo apt install -y ocl-icd-libopencl1 opencl-headers clinfo
+sudo apt install -y opencl-headers ocl-icd-libopencl1 ocl-icd-opencl-dev clinfo
+sudo add-apt-repository ppa:ubuntu-toolchain-r/test -y
+sudo apt-get update -y
+sudo apt-get install g++-7 -y
 sudo apt update -y
 sudo apt upgrade -y
 
-tar -xf computecpp-ce-2.3.0-x86_64-linux-gnu.tar.gz
 
-#tar -xf l_opencl_p_18.1.0.015.tgz
+tar -xf computecpp-ce-2.3.0-x86_64-linux-gnu.tar.gz
 
